bd69c2127cb348fbb878708e6190d3680eb7325f
Sudharsan0897 <134062573+Sudharsan0897@users.noreply.github.com>
2025-12-23 16:49:53 +0530

    Update branch_database.py

--- branch_database.py
+++ branch_database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "postgresql://user_branch_user:WZ1TwBgdPzPhOLpAgZTfNkwe7X2Frj0B@dpg-d553v0m3jp1c739juavg-a.virginia-postgres.render.com/user_branch"
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, echo=True, future=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
